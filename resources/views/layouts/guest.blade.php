945ac519040a1d9d1524d9b66f1f4dd3aee705a5
Star_SNG <starpe.gamer@gmail.com>
2025-06-03 14:51:17 +0700

    Added Log In nav button

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -38,20 +38,26 @@
 		<link rel="preconnect" href="https://fonts.bunny.net">
 		<link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 {{--		<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />--}}
-
+		
 		<!-- Scripts -->
 		@vite(['resources/css/app.css', 'resources/js/app.js'])
 		@fluxAppearance
 	</head>
 	<body class="font-sans text-gray-900 antialiased">
 		<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-black">
+			<div class="absolute top-4 right-4">
+				@if (!request()->routeIs('login'))
+					<flux:link href="{{ route('login') }}" wire:navigate>{{ __('Log in') }}</flux:link>
+				@endif
+			</div>
+			
 			<div>
 				<a href="/" wire:navigate>
 					<x-application-logo class="w-20 h-20 sm:w-28 sm:h-28" />
 				</a>
 			</div>
 			
-			<div class="w-full sm:max-w-md mt-6 px-6 py-4 outline bg-white dark:bg-black dark:outline-red-600 shadow-md overflow-hidden sm:rounded-lg">
+			<div class="w-full sm:max-w-md mt-6 px-6 py-4 outline bg-white dark:bg-black outline-red-600 shadow-md overflow-hidden sm:rounded-lg">
 				{{ $slot }}
 			</div>
 		</div>
--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -44,30 +44,29 @@ new #[Layout('layouts.guest')] class extends Component
 			<flux:input wire:model="form.email" type="email" autofocus required />
 			<flux:error name="form.email" />
 		</flux:field>
-  
+		
 		<!-- Password -->
 		<flux:field class="mt-4">
 			<flux:label>{{ __('Password') }}</flux:label>
 			<flux:input wire:model="form.password" type="password" autofocus autocomplete="current-password" required />
 			<flux:error name="form.password" />
 		</flux:field>
-  
+		
 		<!-- Remember Me -->
 		<flux:field variant="inline" class="mt-4">
 			<flux:checkbox wire:model="form.remember" />
 			<flux:label>{{ __('Remember me') }}</flux:label>
 		</flux:field>
-  
+		
 		<div class="flex items-center justify-end mt-4">
 			@if (Route::has('password.request'))
 				<a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}" wire:navigate>
 					{{ __('Forgot your password?') }}
 				</a>
 			@endif
-   
-			<x-primary-button class="ms-3">
-				{{ __('Log in') }}
-			</x-primary-button>
+			
+{{--			<x-primary-button class="ms-3">{{ __('Log in') }}</x-primary-button>--}}
+			<flux:button variant="primary" type="submit" class="ms-3">{{ __('Log in') }}</flux:button>
 		</div>
 	</form>
 </div>
